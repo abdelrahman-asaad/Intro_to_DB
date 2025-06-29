58492057e73c773972cab936e254d0902fc743af
Asaad <asaad1793.z@gmail.com>
2025-06-29 14:24:17 +0300

    add

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,29 +1,22 @@
 import mysql.connector
-from mysql.connector import Error
 
 try:
-    # Connect to MySQL Server (بدون اختيار قاعدة بيانات)
     connection = mysql.connector.connect(
         host='localhost',
         user='root',
-        password='yourpassword'  # ← غيّرها للباسورد الحقيقي
+        password='yourpassword'  
     )
 
     if connection.is_connected():
         cursor = connection.cursor()
-
-        # Create database if not exists
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
+except mysql.connector.Error as e:
     print("Error while connecting to MySQL:", e)
 
 finally:
-    # Close cursor and connection safely
-    try:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-    except:
-        pass
+    if 'cursor' in locals(): # if cursor in local variables scope 
+        cursor.close()
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
