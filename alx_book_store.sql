6f92cfb2997542d1f26e00f5c5ab3aa15d7fc577
Asaad <asaad1793.z@gmail.com>
2025-06-29 13:26:50 +0300

    added

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -30,6 +30,7 @@ CREATE TABLE IF NOT EXISTS Customers (
 CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
+    quantity DOUBLE,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
